9e578e227a7428ec5ed5a8f1550abd18816e4c02
Stowaway4331 <92860009+Stowaway4331@users.noreply.github.com>
2023-10-14 11:49:29 +0530

    fixed bgColor bug

--- src/controllers/Button.jsx
+++ src/controllers/Button.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import PropTypes from 'prop-types';
 
-const Button = ({bgColor="00c8ce", children }) => {
+const Button = ({bgColor="#00c8ce", children }) => {
   return (
-    <button className={`bg-[${bgColor}] bg-${bgColor} min-w-20 w-max px-4 py-2 rounded-md text-[0.75em] font-bold cursor-pointer text-black hover:scale-105 transition-all`}>
+    <button style={{
+      backgroundColor: bgColor
+    }} className={`bg-[#00c8ce] min-w-20 w-max px-4 py-2 rounded-md text-[0.75em] font-bold cursor-pointer text-black hover:scale-105 transition-all`}>
       {children}
     </button>
   );
